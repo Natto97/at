d44da9047f86ecfbc89c1286b2276f4f5524de50
Natto <start1433@163.com>
2021-04-20 11:37:37 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+wget https://github.com/chaitin/xray/releases/download/1.7.1/xray_linux_amd64.zip
+unzip xray_linux_amd64.zip
+ls
+chmod +x xray_linux_amd64
+git clone https://github.com/Natto97/at
+./xray_linux_amd64 webscan --target-file  at/url.txt 
