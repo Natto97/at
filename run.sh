d2375057cb82603215fb619b5178b188174228da
Natto <start1433@163.com>
2021-04-20 12:42:17 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
+
 wget https://github.com/chaitin/xray/releases/download/1.7.1/xray_linux_amd64.zip
 unzip xray_linux_amd64.zip
 ls
 chmod +x xray_linux_amd64
 git clone https://github.com/Natto97/at
-cp xray_linux_amd64 /usr/xray
-cat  at/url.txt | xray webscan
+
+echo ./xray webscan --url-file at/url.txt > run.sh
+zsh run.sh
